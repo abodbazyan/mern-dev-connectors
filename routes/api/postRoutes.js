59b12ea4643ef6ee74940958a33f0d19811241cb
Abod Bazyan <abod_bazyan646@hotmail.com>
2020-01-18 20:05:57 +0200

    added likes, comments, and posts routes

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -1,9 +1,52 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const postController = require('../../controllers/postController');
+
 const router = express.Router();
 
-// @route   GET   api/post/test
-// @desc    test post route
+// @route   GET   api/post/
+// @desc    get a post
 // @access  public
-router.get("/test", (req, res) => res.json({ msg: "post Works" }));
+router.get('/', postController.getPosts);
+
+// @route   GET   api/post/:id
+// @desc    get post by id
+// @access  public
+router.get('/:id', postController.getPost);
+
+// ***********************************
+// Checking if the user is authenticated or not
+router.use(passport.authenticate('jwt', { session: false }));
+// ***********************************
+
+// @route   POST   api/post/
+// @desc    create new post
+// @access  private
+router.post('/', postController.createPost);
+
+// @route   DELETE   api/post/:id
+// @desc    delete my post
+// @access  private
+router.delete('/:id', postController.deletePost);
+
+// @route   POST   api/post/like/:id
+// @desc    add like to a post
+// @access  private
+router.post('/like/:id', postController.addLike);
+
+// @route   POST   api/post/unlike/:id
+// @desc    remove like from a post
+// @access  private
+router.post('/unlike/:id', postController.removeLike);
+
+// @route   POST   api/post/comment/:id
+// @desc    add a comment to a post
+// @access  private
+router.post('/comment/:id', postController.addComment);
+
+// @route   DELETE   api/post/comment/:id/:comment_id
+// @desc    remove a comment from a post
+// @access  private
+router.delete('/comment/:id/:comment_id', postController.removeComment);
 
 module.exports = router;
